0f03dcfb93cf0a5bde2c750ce322db6ee65d6c45
srinivas <srinivas>
2023-08-28 11:27:42 +0530

    added some new project

---
+++ hrmdemo/hrmdemo/src/main/java/com/demo/hrm/HrmdemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.hrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmdemoApplication.class, args);
+	}
+
+}
---
+++ hrmdemo/hrmdemo/src/test/java/com/demo/hrm/demo/HrmdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.hrm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
